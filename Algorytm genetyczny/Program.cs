e513612e9a6fa1870a8dd1a27199b40a40e0f859
Michał Rdzanek <m33how.cs@gmail.com>
2019-02-07 14:49:24 +0100

    Ready

--- Algorytm genetyczny/Program.cs
+++ Algorytm genetyczny/Program.cs
@@ -9,7 +9,7 @@ namespace Genetyczny_v_1_0
 {
     struct Parametry
     { //parametry symulacji
-        public const int lp = 40;
+        public const int lp = 400;
         //liczba pokoleń w eksperymencie
         public const double a = 3.0;
         //wartość początkowa przestrzeni poszukiwań dla x
@@ -19,17 +19,17 @@ namespace Genetyczny_v_1_0
         //wartość początkowa przestrzeni poszukiwań dla y
         public const double d = 4.0;
         //wartość końcowa przestrzeni poszukiwań dla y
-        public const int N = 22;
+        public const int N = 11;
         //łączna liczba genów w pojedynczym chromosomie
         public const int Nx = 11;
         //liczba genów w pojedynczym chromosomie
         public const int Ny = 11;
         //liczba genów w pojedynczym chromosomie
-        public const int pula = 20;
+        public const int pula = 2000;
         //liczba osobników  w populacji (liczba parzysta)
-        public const double pk = 0.75;
+        public const double pk = 0.95;
         //prawdopodobieństwo krzyżowania
-        public const double pm = 0.01;
+        public const double pm = 0.02;
         //prawdopodobieństwo mutacji
     }
 
@@ -60,48 +60,64 @@ namespace Genetyczny_v_1_0
 
     class Populacja
     {
-        Byte[,] populacja;
+        public Byte[,] populacjaX;
+        public Byte[,] populacjaY;
+
         //dwuwymiarowa tablica aktualnych chromosomów populacji,
         //rozmieszczonych w kolejnych wierszach tablicy
 
         public Populacja()                                     //<-----------------------------------Wprowadzić kolejną populację dla y
         //konstruktor populacji - przydziela pamięć dla bitowej
         //reprezentacji populacji chromosomów
-        { populacja = new Byte[Parametry.pula, Parametry.N]; }
+        {
+            populacjaX = new Byte[Parametry.pula, Parametry.Nx];
+            populacjaY = new Byte[Parametry.pula, Parametry.Ny];
+        }
 
         double[] tablicaFenotypowX = new double[Parametry.pula];
         //tablica wartości fenotypów dla populacji chromosomów X
         double[] tablicaFenotypowY = new double[Parametry.pula];
         //tablica wartości fenotypów dla populacji chromosomów X
 
-        int power = LiczbyBazowe.Power();
+        int powerX = LiczbyBazowe.Power();
+        int powerY = LiczbyBazowe.Power();
 
         double[] tablicaDostosowanie = new double[Parametry.pula];
+        double[] tablicaDostosowanieX = new double[Parametry.pula];
+        double[] tablicaDostosowanieY = new double[Parametry.pula];
         //tablica wartości funkcji dostosowania dla populacji chromosomw
 
-        public void LosujPopulacje()                                  // <---------------------------Wprowadzić kolejną metodę losującą liczby dla x i y
+        public void LosujPopulacje()
         {   //metoda losuje nową populację chromosomów
             //i umieszcza je w lokalnej tablicy 'populacja'
             for (int pozycja = 0; pozycja < Parametry.pula; pozycja++)
             {
                 Random Generator = new Random(LiczbyBazowe.PobierzBazowa());
                 for (int j = 0; j < Parametry.N; j++)
-                    populacja[pozycja, j] = (Byte)Generator.Next(2);
+                    populacjaX[pozycja, j] = (Byte)Generator.Next(2);
+
+            }
+
+            for (int pozycja = 0; pozycja < Parametry.pula; pozycja++)
+            {
+                Random Generator = new Random(LiczbyBazowe.PobierzBazowa());
+                for (int i = 0; i < Parametry.N; i++)
+                    populacjaY[pozycja, i] = (Byte)Generator.Next(2);
             }
         }
 
         int ObliczFenotypChromosomuX(int pozycjaChromosomu)
         {   //metoda liczy reprezentację dziesiętną wskazanego chromosomus
             int fenotyp = 0, rat = 1;
-            for (int j = 0; j < Parametry.Nx; j++)
-            { fenotyp = fenotyp + populacja[pozycjaChromosomu, j] * rat; rat = rat * 2; }
+            for (int j = 0; j < Parametry.N; j++)
+            { fenotyp = fenotyp + populacjaX[pozycjaChromosomu, j] * rat; rat = rat * 2; }
             return fenotyp;
         }
         int ObliczFenotypChromosomuY(int pozycjaChromosomu)
         {   //metoda liczy reprezentację dziesiętną wskazanego chromosomus
             int fenotyp = 0, rat = 1;
             for (int j = Parametry.Nx; j < Parametry.N; j++)
-            { fenotyp = fenotyp + populacja[pozycjaChromosomu, j] * rat; rat = rat * 2; }
+            { fenotyp = fenotyp + populacjaY[pozycjaChromosomu, j] * rat; rat = rat * 2; }
             return fenotyp;
         }
         public void ObliczFenotypyX()                  // <---------------------------------------------------Do przerobienia--------------------------------------
@@ -111,8 +127,8 @@ namespace Genetyczny_v_1_0
             //power = Parametry.Nx;
             for (int pozycja = 0; pozycja < Parametry.pula; pozycja++)
                 tablicaFenotypowX[pozycja] = Parametry.a + (Parametry.b - Parametry.a)    // <------------------------Dodać zmienne ?
-                * ObliczFenotypChromosomuX(pozycja) / power;
-                power = Parametry.Nx;
+                * ObliczFenotypChromosomuX(pozycja) / powerX;
+                //powerX = Parametry.Nx;
         }
         public void ObliczFenotypyY()                  // <---------------------------------------------------Do przerobienia--------------------------------------
         {   //metoda liczy wartości fenotypów chromosomów populacji
@@ -121,8 +137,8 @@ namespace Genetyczny_v_1_0
           //  power = Parametry.N - Parametry.Nx;
             for (int pozycja = 0; pozycja < Parametry.pula; pozycja++)
                 tablicaFenotypowY[pozycja] = Parametry.c + (Parametry.d - Parametry.c)    // <------------------------Dodać zmienne ?
-                * ObliczFenotypChromosomuY(pozycja) / power;
-              power = Parametry.N - Parametry.Nx;
+                * ObliczFenotypChromosomuY(pozycja) / powerY;
+              //power = Parametry.N - Parametry.Nx;
         }
 
         public void ObliczDostosowanie()
@@ -157,29 +173,17 @@ namespace Genetyczny_v_1_0
         public void Ruletka()
         {   //selekcja chromosomów w populacji metodą koła ruletki
 
-            Byte[,] nowePokolenie = new Byte[Parametry.pula, Parametry.N];
+            Byte[,] nowePokolenieX = new Byte[Parametry.pula, Parametry.N];
+            Byte[,] nowePokolenieY = new Byte[Parametry.pula, Parametry.N];
             //tablica pomocnicza chromosomów dla ruletki
 
-            double[] tablicaNI = new double[Parametry.pula];
-            //tablica pomocnicza ruletki
-
-            DostosowanieNormalizacja();
-
-            double sumaDostosowanie = 0;
-            foreach (double dostosowanie in tablicaDostosowanie)
-                sumaDostosowanie += dostosowanie;
-
-            for (int i = 0; i < Parametry.pula; i++)
-            {
-                tablicaNI[i] = tablicaDostosowanie[i]                   // <------------- Przypisanie tablicy dostosowania do tablicaNI
-                               / sumaDostosowanie * power;
-            }
+            double[] tablicaNI = sumaDostosowanie();
 
             int[] losowe = new int[Parametry.pula];
             //tabela 'losowe' przechowuje liczby losowe z przedziału 0...Power()
             Random Generator = new Random(LiczbyBazowe.PobierzBazowa());
             for (int i = 0; i < Parametry.pula; i++)
-                losowe[i] = Generator.Next(power);
+                losowe[i] = Generator.Next(LiczbyBazowe.Power());
 
             double[] ruletka = new double[Parametry.pula];
             //tablica pozycji wycinków ruletki
@@ -196,12 +200,37 @@ namespace Genetyczny_v_1_0
                 int j = 0;
                 while (losowe[i] > ruletka[j]) j++;
                 for (int k = 0; k < Parametry.N; k++)
-                    nowePokolenie[i, k] = populacja[j, k];
+                {
+                    nowePokolenieX[i, k] = populacjaX[j, k];
+                }
+                for (int m = 0; m < Parametry.N; m++)
+                {
+                    nowePokolenieY[i, m] = populacjaY[j, m];
+                }
             }
-            populacja = nowePokolenie;
+            populacjaX = nowePokolenieX;
+            populacjaY = nowePokolenieY;
         }
 
-        void Krzyzowanie()
+        double[] sumaDostosowanie()
+        {
+            //tablica pomocnicza ruletki
+            double[] tablicaNI = new double[Parametry.pula];
+            double sumaDostosowanie = 0;
+            foreach (double dostosowanie in tablicaDostosowanie)
+                sumaDostosowanie += dostosowanie;
+
+            for (int i = 0; i < Parametry.pula; i++)
+            {
+                tablicaNI[i] = tablicaDostosowanie[i]
+                               / sumaDostosowanie * LiczbyBazowe.Power();
+            }
+
+            return tablicaNI;
+        }
+
+
+        void Krzyzowanie(Byte[,] populacja, int parametryN)
         {
             Random Generator = new Random(LiczbyBazowe.PobierzBazowa());
             //tworzy generator liczb losowych oparty o kolejną
@@ -216,7 +245,7 @@ namespace Genetyczny_v_1_0
             //losowanie miejsc krzyżowania dla par
             int[] losoweMiejsca = new int[liczbaPar];
             for (int i = 0; i < liczbaPar; i++)
-                losoweMiejsca[i] = Generator.Next(Parametry.N - 2);
+                losoweMiejsca[i] = Generator.Next(parametryN - 2);
 
             //proces krzyżowania genów w parach
             int pierwszy = 0; //indeks pierwszego osobnika w każdej parze
@@ -224,7 +253,7 @@ namespace Genetyczny_v_1_0
             for (int para = 0; para < liczbaPar; para++)
             {
                 if (losowePary[para] < Parametry.pk * 100)
-                    for (int i = losoweMiejsca[para]; i < Parametry.N; i++)
+                    for (int i = losoweMiejsca[para]; i < parametryN; i++)
                     {
                         bufor = populacja[pierwszy, i];
                         populacja[pierwszy, i] = populacja[pierwszy + 1, i];
@@ -244,7 +273,7 @@ namespace Genetyczny_v_1_0
             foreach (double fenotyp in tablicaFenotypowY)
                 Console.Write("{0:#.##}\n Dla Y -> ", fenotyp);
         }
-        public void Mutacje()
+        public void Mutacje(Byte[,] populacja, int parametryN)
         {  //metoda losuje chromosomy do mutacji 
            //i mutuje losowe geny w wylosowanych chromosomach
 
@@ -261,12 +290,16 @@ namespace Genetyczny_v_1_0
             for (int i = 0; i < Parametry.pula; i++)
                 if (losowe[i] < Parametry.pm)
                 {
-                    miejsceMutacji = Generator.Next(Parametry.N);
+                    miejsceMutacji = Generator.Next(parametryN);
                     if (populacja[i, miejsceMutacji] == 0)
                         populacja[i, miejsceMutacji] = 1;
                     else populacja[i, miejsceMutacji] = 0;
+
+
                 }
         }
+
+
 
         public void PokazDostosowaniePopulacji()
         {   //wyświetla wartości funkcji dostosowania 
@@ -275,7 +308,7 @@ namespace Genetyczny_v_1_0
                 Console.Write("{0:#.##}\n ","\t", dostosowanie);
         }
 
-        void PokazChromosomyPopulacji()
+        void PokazChromosomyPopulacji(Byte[,] populacja)
         {
             for (int i = 0; i < populacja.GetLength(0); i++)
             {
@@ -286,6 +319,8 @@ namespace Genetyczny_v_1_0
                 Console.WriteLine();
             }
         }
+
+
 
         public double ObliczDostosowanieSrednie()
         {
@@ -322,15 +357,17 @@ namespace Genetyczny_v_1_0
                 {
                     nrPokolenia++;
                     populacja.Ruletka();
-                    populacja.Krzyzowanie();
-                    populacja.Mutacje();
+                    populacja.Krzyzowanie(populacja.populacjaX, Parametry.N);
+                    populacja.Krzyzowanie(populacja.populacjaY, Parametry.N);
+                    populacja.Mutacje(populacja.populacjaX, Parametry.N);
+                    populacja.Mutacje(populacja.populacjaY, Parametry.N);
                     populacja.ObliczFenotypyX();
                     populacja.ObliczFenotypyY();
                     populacja.ObliczDostosowanie();
                     Console.Write("{0, 3}          ", nrPokolenia);
                     populacja.PokazDostosowanieSrednie();
-                 //   populacja.PokazFenotypyPopulacjiX();
-                //    populacja.PokazFenotypyPopulacjiY();
+                 //  populacja.PokazFenotypyPopulacjiX();
+                  //  populacja.PokazFenotypyPopulacjiY();
                 //    Console.WriteLine("\n\nPoniżej kolejne pokolenie\n");
                 }
                 Console.ReadKey();
